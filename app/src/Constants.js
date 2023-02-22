6f5ea4c069ab950df298279f2d9bcf5e65017568
Prajapat, Laxmi <laxmi.prajapat22@imperial.ac.uk>
2023-02-22 23:19:31 +0000

    SWEG-81 GCP deployment for front-end

---
+++ app/src/Constants.js
@@ -0,0 +1,11 @@
+const prod = {
+    url: {
+        API_URL: 'https://foodsnap-backend-qstt5gdgeq-nw.a.run.app'
+    }
+};
+const dev = {
+    url: {
+        API_URL: 'http://127.0.0.1:5000'
+    }
+};
+export const config = process.env.NODE_ENV === 'development' ? dev : prod;
