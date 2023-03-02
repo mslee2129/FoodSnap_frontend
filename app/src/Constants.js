68b247affcb2c77da9d8dd1e2d543d9686569616
Ryan Perkins <ryan.perkins21@imperial.ac.uk>
2023-03-02 16:04:00 +0000

    Sweg 126 help button

--- app/src/Constants.js
+++ app/src/Constants.js
@@ -5,8 +5,8 @@ const prod = {
 };
 const dev = {
     url: {
-        //API_URL: 'http://127.0.0.1:5000'
-        API_URL: 'https://foodsnap-backend-qstt5gdgeq-nw.a.run.app'
+        API_URL: 'http://127.0.0.1:5000'
+        //API_URL: 'https://foodsnap-backend-qstt5gdgeq-nw.a.run.app'
     }
 };
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
--- app/src/elements/Hover/Hover.js
+++ app/src/elements/Hover/Hover.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Hover.css'
 
 function Hover() {
-  return (
-    <div className="HoveringPage">
-      <div className="HoveringPageContent">
-          <h3>Welcome to FoodSnap!</h3>
-          <p>FoodSnap is a progressive web app that allows you to upload/take a picture of a food item to estimate its nutritional value.
-            To get started, upload a picture of your food.</p>
-          <button className="CloseButton">X</button>
-      </div>
-    </div>
+
+    const [showHover, setShowHover] = useState(true);
+
+    const handleClose = () => {
+        setShowHover(false);
+    };
+
+    return (
+        <>
+            {showHover && (
+                <div className="HoveringPage">
+                    <div className="HoveringPageContent">
+                        <h3>Welcome to FoodSnap!</h3>
+                        <p>
+                            FoodSnap is a progressive web app that allows you to upload/take a
+                            picture of a food item to estimate its nutritional value. To get
+                            started, upload a picture of your food.
+                        </p>
+                        <button className="CloseButton" onClick={handleClose}>
+                            X
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
   
